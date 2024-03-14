5dc57b0f0bf824cf4643593a0d8a2b2d5834e065
Mubashir12392 <mubashirhaider01@gmail.com>
2024-03-14 15:01:50 +0500

    add property decorators in descriptors file

--- OOP/descriptors.py
+++ OOP/descriptors.py
@@ -31,6 +31,9 @@
 
 
 
+from typing import Any
+
+
 class Descriptor:
     def __init__(self) -> None:
         self.__fuel_cap = 0
@@ -67,6 +70,13 @@ class Car:
         self.model = model
         self.fuel_cap = fuel_cap
 
+    # You can also set descriptor conditon by using this magic method
+
+    # def __setattr__(self, name,value) -> None:
+    #     if name == 'make' and not isinstance(value, str):
+    #         raise TypeError('Make of the car must be str')
+    #     super().__setattr__(name,value)
+
 
     def __str__(self) -> str:
         return f'{self.make} company car model {self.model} has fuel capacity of {self.fuel_cap} litres'
@@ -79,3 +89,85 @@ print(id1)
 # You might use a class descriptor if you're implementing a database ORM where you need to lazily 
 # load data, perform validation, or trigger cascading updates upon attribute access, setting, or deletion.
 # It is used when attributes are not private, if private use property decorator
+
+
+
+# ___________________________________Property Decorators_______________________________
+
+
+# The property decorator in Python is a built-in feature that allows you to define getter, setter, and 
+# deleter methods for encapsulated instance attributes and class attributes.
+# It provides a way to encapsulate attribute access, enabling you to execute custom logic when getting,
+# setting, or deleting attributes.
+
+# # Getter method
+# class Student:
+#     def __init__(self,name) -> None:
+#         self.__name = name
+
+#     @property
+#     def name(self):                    # Main property method
+#         return self.__name
+    
+
+#     # Setter method
+#     @name.setter                       # This name will always same to the main property method
+#     def name_change(self, name):
+#         self.__name = name
+
+
+#     # Deleter method
+#     @name.deleter                    # This name will always same to the main property method
+#     def delete_name(self):
+#         del self.__name
+
+
+
+# When you define a method as a property using the @property decorator, 
+# it essentially transforms that method into a descriptor instance. 
+# Descriptors provide a way to define how attribute access works on a class, 
+# allowing you to customize behavior when accessing, setting, or deleting attributes.
+# How to call these methods
+# You should call these methods without parenthesis(), with the method name 
+# These property decoraters allow us to use Encapsulated attributes like the normal attributes
+        
+
+# a = Student('Ali')
+# print(a.name)
+# a.name_change = 'Mubashir'      
+# print(a.name)
+# del a.delete_name
+
+
+# You can also convert normal methods to property Methods
+
+# class Teacher:
+#     def __init__(self,name,age) -> None:
+#         self.__name = name
+#         self.__age = age
+
+#     def get_name(self):
+#         return self.__name
+    
+#     def update_name(self, new_name):
+#         self.__name = new_name
+
+#     def delete_name(self):
+#         del self.__name
+
+#     # Convert all methods into property methods
+#     name = property(get_name, update_name, delete_name)
+
+# obj1 = Teacher('Ambreen',25)
+# obj2 = Teacher('Themina',23)
+
+# obj1.name = 'Shumaila'
+# print(obj1.name)
+# del obj2.name
+# print(obj2.name)
+
+
+#NOTE
+# You might use a property decorator if you're implementing a simple data container 
+# class where you want to ensure that certain attributes are always accessed or modified 
+# through getter and setter methods. It is used when attributes are private
